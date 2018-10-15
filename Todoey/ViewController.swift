1c181ef06ec32e9609f0f6bf0db002a9c86b540c
Wojtek Nalewajko <wnalewajko@gmail.com>
2018-10-15 15:20:20 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Wojciech Nalewajko on 15/10/2018.
+//  Copyright © 2018 Wojciech Nalewajko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
